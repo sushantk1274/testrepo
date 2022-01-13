c2a6ebb1809e78cd188ec692dc74de8d1776d1a6
Sushant <ksushantvmc@gmail.com>
2022-01-13 13:37:26 +0530

    Added folder

---
+++ testfolder/one.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout<<"Hello world under testfolder to refresh my git skills";
+	return 0;
+}
+// ctrl x is used to save files in nano
+// step 1 : press ctrl x and then press y
+// and then click enter 
+
